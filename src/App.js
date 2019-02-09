79ece2f28ac8fe4b05024eac77a714ba5ddf4f4b
Jon Q <hello@jonquach.com>
2019-02-09 13:44:58 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { withTransition } from "./lib";
+import "./App.css";
+
+const NavBar = () => (
+  <div>
+    <NavLink to="/">Page One</NavLink>
+    <NavLink to="/two">Page Two</NavLink>
+    <NavLink to="/three">Page Three</NavLink>
+  </div>
+);
+
+const One = () => (
+  <div className="slide one">
+    <div>One</div>
+  </div>
+);
+const Two = () => (
+  <div className="slide two">
+    <div>Two</div>
+  </div>
+);
+const Three = () => (
+  <div className="slide three">
+    <div>Three</div>
+  </div>
+);
 
 class App extends Component {
+  state = {
+    effect: "fadeAndPop"
+  };
+
+  effects = [
+    "drop",
+    "fadeAndMove",
+    "fadeAndPop",
+    "moveIn",
+    "pop",
+    "scale",
+    "zoom"
+  ];
+
+  changeEffect = event => {
+    const effect = event.target.value;
+    this.setState({ effect });
+  };
+
   render() {
+    const { effect } = this.state;
+
+    const PlayOne = withTransition({ effect })(One);
+    const PlayTwo = withTransition({ effect })(Two);
+    const PlayThree = withTransition({ effect })(Three);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Router>
+        <div className="App">
+          <h1>▶️ Press Play</h1>
+          <p>A simple transition library for React (and routers!)</p>
+          Effect:
+          <select value={this.state.effect} onChange={this.changeEffect}>
+            {this.effects.map(effect => (
+              <option
+                value={effect}
+                key={effect}
+                checked={effect === this.state.effect}
+              >
+                {effect}
+              </option>
+            ))}
+          </select>
+          <hr />
+          <NavBar />
+          <hr />
+          <Route path="/" exact component={PlayOne} />
+          <Route path="/two" component={PlayTwo} />
+          <Route path="/three" component={PlayThree} />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Made By <a href="https://jonquach.com">Q</a>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
